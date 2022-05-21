f82666f99cc446d8a5b89d09227085966a880acb
BastyRioT <ba.alvaradog@duocuc.cl>
2022-05-20 20:10:02 -0400

    Se hizo la página web, se agregó la categoria a la api

--- tienda_musicpro/tiendita/urls.py
+++ tienda_musicpro/tiendita/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path,include,re_path
-from .views import index
+from django.urls import path, include, re_path
+from .views import index, album, login
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns =[
-   path('',index, name='index'),
+   path('', index, name='index'),
+   path('album/', album, name='album'),
+   path('contact/', login, name='contact'),
     
 ]
 if settings.DEBUG:urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tienda_musicpro/tiendita/views.py
+++ tienda_musicpro/tiendita/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):     
     return render(request,'index.html')
+
+def album(request):     
+    return render(request,'album.html')
+
+def login(request):     
+    return render(request,'contact.html')
